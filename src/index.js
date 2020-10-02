ea531a804b1345d7ec596c8d38dfea262093d1d7
Leonid Rusakov <bengunn87@mail.ru>
2020-10-02 21:31:28 +0500

    feat: complete human readable number function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function toReadable (number) {
-  
+    const numbers = [
+        '', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 
+        'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'
+    ];
+
+    const dozens = {
+        2: 'twenty',
+        3: 'thirty',
+        4: 'forty',
+        5: 'fifty',
+        6: 'sixty',
+        7: 'seventy',
+        8: 'eighty',
+        9: 'ninety'
+    }
+    if (number === 0) {
+        return 'zero';
+    } 
+    
+    if (number <= 19) {
+        return numbers[number];
+    }
+
+    const numbersArray = ('' + number).split('');
+    if (numbersArray.length === 2) {
+        return `${dozens[numbersArray[0]]} ${numbers[numbersArray[1]]}`.trim();
+    }
+
+    let humanReadableNumber = `${numbers[numbersArray[0]]} hundred `;
+    number = number % 100;
+    humanReadableNumber += (number <= 19 )
+        ? numbers[number]
+        : `${dozens[numbersArray[1]]} ${numbers[numbersArray[2]]}`
+    ;
+    return humanReadableNumber.trim();
 }
